19eee410781813069569d8c50b4d826de09ecfd7
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-03-18 21:21:51 +0000

    Update main.b28c5c68.chunk.js

--- static/js/main.b28c5c68.chunk.js
+++ static/js/main.b28c5c68.chunk.js
@@ -3286,7 +3286,7 @@
                     {
                         pid: 4,
                         lpSymbol: "HACHIKO-HACHIKOBSC LP",
-                        lpAddresses: { 97: "", 56: "0X3eaF4F37624dF9ca859f18e55D26232d076F30df" },
+                        lpAddresses: { 97: "", 56: "0x3eaF4F37624dF9ca859f18e55D26232d076F30df" },
                         tokenSymbol: "HACHIKO",
                         tokenAddresses: { 97: "", 56: "0x3ae20ee46822efad0099a40be21a36057914d963" },
                         quoteTokenSymbol: i.b.BNB,
